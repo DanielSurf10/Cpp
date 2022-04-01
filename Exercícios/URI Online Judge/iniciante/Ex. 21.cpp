49c4ae378aa1e6c76915e39195a1eb9f521b2dcc
DanielSurf10 <daniel101surf@gmail.com>
2022-04-01 16:14:25 -0300

    Novas coisas adicionadas em "coisas de C++"

--- Exercícios/URI Online Judge/iniciante/Ex. 21.cpp
+++ Exercícios/URI Online Judge/iniciante/Ex. 21.cpp
@@ -22,50 +22,16 @@ int main() {
         cout << num2 << " notas(s) de R$ " << setprecision(2) << (float)notas[i] << endl;
     }
 
-    // // Nota de 100
-    // num2 = num / 100;
-    // num -= num2 * 100;
-    // cout << num2 << " nota(s) de R$ 100.00" << endl;
-
-    // // Nota de 50
-    // num2 = num / 50;
-    // num -= num2 * 50;
-    // cout << num2 << " nota(s) de R$ 50.00" << endl;
-
-    // // Nota de 20
-    // num2 = num / 20;
-    // num -= num2 * 20;
-    // cout << num2 << " nota(s) de R$ 20.00" << endl;
-
-    // // Nota de 10
-    // num2 = num / 10;
-    // num -= num2 * 10;
-    // cout << num2 << " nota(s) de R$ 10.00" << endl;
-
-    // // Nota de 5
-    // num2 = num / 5;
-    // num -= num2 * 5;
-    // cout << num2 << " nota(s) de R$ 5.00" << endl;
-
-    // // Nota de 2
-    // num2 = num / 2;
-    // num -= num2 * 2;
-    // cout << num2 << " nota(s) de R$ 2.00" << endl;
-
-
     // Moedas
     cout << "MOEDAS:" << endl;
 
-    for (int i = 0; i < 6; i++) {
-        cout << num << " / " << moedas[i] << " = " << num / moedas[i] << " --- ";
+    for (int i = 0; i < 5; i++) {
         num2 = num / moedas[i];
-        cout << num << " - " << num2 * moedas[i] << " = " << num - num2 * moedas[i] << " --- ";
         num -= num2 * moedas[i];
-        cout << num2 << " moeda(s) de R$ " << setprecision(2) << moedas[i] << " =- " << num << endl;
+        cout << num2 << " moeda(s) de R$ " << setprecision(2) << moedas[i] << endl;
     }
 
-    // // 1 real
-    // cout << num << " moeda(s) de R$ 1.00" << endl;
+    cout << setprecision(0) << num * 100 << " moeda(s) de R$ " << setprecision(2) << moedas[5] << endl;
 
 
     return 0;
